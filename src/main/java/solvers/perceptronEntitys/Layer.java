71610c1d87fe0e2516d467c67efffadaf1518b64
Regmir <arteom.lap@yandex.ru>
2018-11-11 13:57:22 +0300

    Test

--- src/main/java/solvers/perceptronEntitys/Layer.java
+++ src/main/java/solvers/perceptronEntitys/Layer.java
@@ -36,7 +36,7 @@ public class Layer implements Serializable {
 
     public double[] calculate (double[] input){
         double[] output = new double[neuronCount];
-        for(int i = 0; i < neurons.length; i++) {
+        for(int i = 1; i < neurons.length; i++) {
             output[i] = neurons[i].calculate(input,activationFunction);
         }
         return  output;
